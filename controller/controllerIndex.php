d9aba346d5d775d4b31f5495d6a8b56b3873e813
LuisDamian <eltelnich@gmail.com>
2021-11-04 22:40:14 +0100

    mvc

---
+++ controller/controllerIndex.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ControllerIndex
+ *
+ * @author Luis Damian
+ * 
+ */
+
+
+// if(isset($_POST["nombrePost"])){
+    
+// }
+include_once "./view/portafolio.php";
+
+?>
